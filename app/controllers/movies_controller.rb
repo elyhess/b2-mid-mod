90922dfa51bdd8ec6a15ed7d88b9b7b84c274b75
Ely Hess <Ely.hess@me.com>
2021-01-04 11:34:45 -0700

    Add actors_avg_age logic

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,5 +2,12 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     @actors = Actor.not_in_movies
+
+    if @movie.actors.empty?
+      @actors_avg_age = 0
+    else
+      @actors_avg_age = @movie.actors.average_age.round(2)
+    end
+
   end
 end
